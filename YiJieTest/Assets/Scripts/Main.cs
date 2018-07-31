d72e8cd6fb1d64c2a7a2715a1a16ad5931f6dea8
aa <aa@youkia.com>
2018-07-31 17:43:43 +0800

    提交test项目

---
+++ YiJieTest/Assets/Scripts/Main.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Button loginButton = transform.Find("LoginButton").GetComponent<Button>();
+        loginButton.onClick.AddListener(ClickLogin);
+    }
+
+    private void ClickLogin()
+    {
+
+    }
+}
---
+++ YiJieTest/Assets/Scripts/SdkManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class SdkManager : MonoBehaviour
+{
+    public static SdkManager INSTANCE;
+
+    void Awake()
+    {
+        INSTANCE = this;
+        using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            using (AndroidJavaObject curActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                setLoginListener(curActivity.GetRawObject(), "Login", "LoginResult");
+            }
+        }
+    }
+
+    public void CreateResult(string message)
+    {
+        Debug.Log("CreateResult=" + message);
+    }
+
+    [DllImport("gangaOnlineUnityHelper")]
+    private static extern void login(IntPtr context, string customParams);
+
+    [DllImport("gangaOnlineUnityHelper")]
+    private static extern void setLoginListener(IntPtr context, string gameObject, string listener);
+
+    public void Login(string customParams)
+    {
+        using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            using (AndroidJavaObject curActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                login(curActivity.GetRawObject(), "Login");
+            }
+        }
+    }
+}
